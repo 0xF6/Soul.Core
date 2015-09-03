f641d038348aecb957c5d2f44d0d1f13e03a67b5
Yayoi Wesp <ls-micro@ya.ru>
2015-09-03 16:38:12 +0300

    edt Regex

--- Soul.Core/Rex/RegexBase.cs
+++ Soul.Core/Rex/RegexBase.cs
@@ -19,7 +19,7 @@ namespace Soul.Core.Rex
         public class PreProc
         {
             public const string IncludeAssembly = "(^#(include )(<[a-zA-Z].*\\.dll>))";     // Include all Assembly
-            public const string IcludeSoulHeader = "(^#(include )(['][a-zA-Z].*\\.sh[']))"; // Include all Soul Header
+            public const string IcludeSoulHeader = "(^#(include )(['][a-zA-Z].*\\.sh[']|[\"][a-zA-Z].*\\.sh[\"]))"; // Include all Soul Header
         }
         public const string MultiLine = "(?m)";
         //x public const string REX_BRECKET_SQAER_MULTILINE = "(?<=\\[)[^\\[\\]]+(?=\\])";
